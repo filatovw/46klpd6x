e996cdeb800c0c962cf1f4ed5e84156cc5343e1b
Vadim Filatov <vadim.filatov@dataart.com>
2021-01-31 21:50:45 +0300

    [CC-2] connected to postgres from the API app

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,22 +9,29 @@ import (
 
 	"github.com/filatovw/46klpd6x/app/api"
 	"github.com/filatovw/46klpd6x/app/api/config"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/joho/godotenv"
 	"go.uber.org/zap"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// setup logger
 	logger, err := zap.NewProduction()
 	if err != nil {
 		fmt.Printf("can't init logger: %s", err)
 		os.Exit(1)
 	}
 	defer logger.Sync()
-
-	sigC := make(chan os.Signal, 1)
-	signal.Notify(sigC, os.Interrupt)
 	sugar := logger.Sugar()
 
+	// handle Ctrl-C
+	sigC := make(chan os.Signal, 1)
+	signal.Notify(sigC, os.Interrupt)
+
+	// load .env vars
 	if err := godotenv.Load(); err != nil {
 		sugar.Infof(".env file was not found: %s", err)
 	}
@@ -32,11 +39,21 @@ func main() {
 	ctx := context.Background()
 	ctx, cancelFn := context.WithCancel(ctx)
 
+	// load api config
 	config, err := config.Load()
 	if err != nil {
 		sugar.Fatalf("failed to get config for application")
 	}
 
+	configPG, _ := pgx.ParseConfig(os.Getenv("DATABASE_URL"))
+	conn := stdlib.OpenDB(*configPG)
+	defer conn.Close()
+	gormConn, err := gorm.Open(postgres.New(postgres.Config{Conn: conn}), &gorm.Config{})
+	res := ""
+	gormConn.Raw("select 1").Find(&res)
+	sugar.Infof("HELLO %+v\n", res)
+
+	// create api server
 	api := api.New(ctx, sugar, config)
 	go func() {
 		if err := api.Serve(); err != nil {
--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/filatovw/46klpd6x
 go 1.15
 
 require (
+	github.com/jackc/pgx/v4 v4.10.1
 	github.com/joho/godotenv v1.3.0
 	go.uber.org/zap v1.16.0
+	gorm.io/driver/postgres v1.0.7
+	gorm.io/gorm v1.20.12
 )
